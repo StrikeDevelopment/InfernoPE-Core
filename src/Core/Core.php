860513ee01f6d4b88dd33b6abd9d585cdae106c8
InfernoPE - Team <34803071+InfernoPE@users.noreply.github.com>
2018-01-04 12:04:18 -0600

    Update Core.php

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -4,8 +4,9 @@ namespace Core;
 
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as C;
+
 use Core\Commands\SimpleCratesCommand;
-use pocketmine\utils\TextFormat as c;
 use Core\SimpleCrates\SimpleCrates;
 
 class Main extends PluginBase implements Listener{
@@ -18,7 +19,7 @@ class Main extends PluginBase implements Listener{
      * registers Commands
      */
     public function registerCommands(){
-        $this->getServer()->getCommandMap()->register("givecratekey", new Commands\SimpleCratesCommand());
+        $this->getServer()->getCommandMap()->register("givekey", new Commands\SimpleCratesCommand());
     }
     public function registerEvents(){
         $this->getServer()->getPluginManager()->registerEvents((new SimpleCrates($this)), $this);
