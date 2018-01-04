e2c4ae21b6340f9a9fccdbe68e03b42be783cf50
InfernoPE - Team <34803071+InfernoPE@users.noreply.github.com>
2018-01-04 12:08:00 -0600

    Update Core.php

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -4,10 +4,15 @@ namespace Core;
 
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+
 use pocketmine\utils\TextFormat as C;
 
 use Core\Commands\SimpleCratesCommand;
 use Core\SimpleCrates\SimpleCrates;
+
+use Core\Potions\Potions;
+use Core\CustomPotion\CustomPotionEvent;
+use Core\Commands\CustomPotion;
 
 class Main extends PluginBase implements Listener{
     
@@ -19,9 +24,12 @@ class Main extends PluginBase implements Listener{
      * registers Commands
      */
     public function registerCommands(){
-        $this->getServer()->getCommandMap()->register("givekey", new Commands\SimpleCratesCommand());
+        $this->getServer()->getCommandMap()->register("givekey", new Commands\Simple());
+        $this->getServer()->getCommandMap()->register("custompotion", new CustomPotion("custompotion", $this));
     }
     public function registerEvents(){
         $this->getServer()->getPluginManager()->registerEvents((new SimpleCrates($this)), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new CustomPotionEvent($this), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new Potions($this), $this);
     }
 }
