1eee9c63c5e45eeb3defd0b5ce3210cf246fd51a
EmeraldMC <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-11-23 15:57:20 -0600

    Update Heal.php

--- src/Core/Commands/Heal.php
+++ src/Core/Commands/Heal.php
@@ -11,7 +11,7 @@ use pocketmine\command\PluginCommand;
 
 use pocketmine\utils\TextFormat as C;
 
-use Core\Main;
+use Core\Loader;
 
 class Heal extends PluginCommand{
 
@@ -25,13 +25,13 @@ class Heal extends PluginCommand{
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
                     if($sender instanceof Player){
                     if (!$sender->hasPermission("core.heal")) {
-                        $sender->sendMessage(C::RED . "You are not allow to do that.");
+                        $sender->sendMessage(Core::PERM_RANK);
                         return false;
                     }
                         $sender->setHealth(20);
                         $sender->sendMessage(C::GREEN . "You have been healed.");
         }else{
-          $sender->sendMessage(C::RED . "You are not In-Game.");
+          $sender->sendMessage(Core::USE_IN_GAME);
         }
             return true;
     }
